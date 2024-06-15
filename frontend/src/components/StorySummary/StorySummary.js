ea75ab33bdeae4f028033f12dc7d513ffd249fa8
aneeshgupta25 <aneeshgupta2512@gmail.com>
2024-06-16 05:22:45 +0530

    Story details connection

--- frontend/src/components/StorySummary/StorySummary.js
+++ frontend/src/components/StorySummary/StorySummary.js
@@ -1,26 +1,29 @@
 import React from "react";
 import styles from "./StorySummary.module.css";
+import { Link } from "react-router-dom";
 
 const StorySummary = (props) => {
   // const { Top, Label1, Middle, backgroundImage } = props;
   console.log(props);
   if (props == {}) return <div></div>;
   return (
-    <div className={styles.myComponent}>
-      <img
-        src={props.image}
-        alt="Background"
-        className={styles.backgroundImage}
-        style={{ opacity: 0.3 }}
-      />
-      <div className={styles.content}>
-        <div className={styles.topLeftText}>{props.title}</div>
-        <div className={styles.labelsContainer}>
-          <span className={styles.label}>{props.label}</span>
+    <Link to={`/user/story/${props._id}`}>
+      <div className={styles.myComponent}>
+        <img
+          src={props.image}
+          alt="Background"
+          className={styles.backgroundImage}
+          style={{ opacity: 0.3 }}
+        />
+        <div className={styles.content}>
+          <div className={styles.topLeftText}>{props.title}</div>
+          <div className={styles.labelsContainer}>
+            <span className={styles.label}>{props.label}</span>
+          </div>
+          <div className={styles.middleLeftText}>{props.desc}</div>
         </div>
-        <div className={styles.middleLeftText}>{props.desc}</div>
       </div>
-    </div>
+    </Link>
   );
 };
 

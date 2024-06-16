b3193dc2cb8b16b1f949087ad32131a3f84c346b
aneeshgupta25 <aneeshgupta2512@gmail.com>
2024-06-16 06:21:19 +0530

    alert

--- frontend/src/components/StoryDetails/StoryDetails.js
+++ frontend/src/components/StoryDetails/StoryDetails.js
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from "react";
 import styles from "./StoryDetails.module.css"; // Import the CSS Module
-import { Link, Navigate, useParams } from "react-router-dom";
+import { Link, Navigate, useNavigate, useParams } from "react-router-dom";
+import RoundButton from "../Button/Button";
 
 const StoryDetails = () => {
+  const navigate = useNavigate();
+  function sendConnectionRequest() {
+    if (
+      window.confirm("You don't have enough skill sets to contact this mentor")
+    ) {
+      navigate("/courses");
+    } else {
+    }
+  }
   const user = {
     name: "John Doe",
     age: 30,
@@ -20,35 +30,36 @@ const StoryDetails = () => {
   };
 
   return (
-    <Link to="/">
-      <div className={styles.profileContainer}>
-        <img
-          className={styles.profileContainerImg}
-          src={user.backgroundImage}
-          alt="background image"
-        />
-        <div className={styles.detailsContainer}>
-          <div className={styles.userDetails}>
-            <p>
-              <strong>Name:</strong> {user.name}
-            </p>
-            <p>
-              <strong>Age:</strong> {user.age}
-            </p>
-            <p>
-              <strong>Location:</strong> {user.location}
-            </p>
-          </div>
+    <div className={styles.profileContainer}>
+      <img
+        className={styles.profileContainerImg}
+        src={user.backgroundImage}
+        alt="background image"
+      />
+      <div className={styles.detailsContainer}>
+        <div className={styles.userDetails}>
+          <p>
+            <strong>Name:</strong> {user.name}
+          </p>
+          <p>
+            <strong>Age:</strong> {user.age}
+          </p>
+          <p>
+            <strong>Location:</strong> {user.location}
+          </p>
+        </div>
+        <div className={styles.profilePictureAndConnect}>
           <div className={styles.profilePicture}>
             <img src={user.profilePicture} alt="User Profile" />
           </div>
+          <RoundButton text="CONNECT" onClick={sendConnectionRequest} />
         </div>
-        <div
-          className={styles.userDescription}
-          dangerouslySetInnerHTML={{ __html: user.description }}
-        ></div>
       </div>
-    </Link>
+      <div
+        className={styles.userDescription}
+        dangerouslySetInnerHTML={{ __html: user.description }}
+      ></div>
+    </div>
   );
 };
 
